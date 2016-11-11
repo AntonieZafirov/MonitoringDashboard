3cf8340d9b820b7446871bc0816b8443daa356e2
azafirov <zafirovantonie@hotmail.com>
2016-11-11 13:40:31 +0100

    interval added

--- src/main/resources/static/js/hChartDash.js
+++ src/main/resources/static/js/hChartDash.js
@@ -3,19 +3,21 @@
  */
 myApp.controller('monitoringDashC', function($scope, $http, $log, $timeout, $interval) {
 
-    $interval(function () {
-        console.log('fetch data');
+    function loadData() {
         $http.get(proxyUrl + "/sonar-service" + "/sonar/widgets/1/chart").then(function (response) {
             var options = response.data;
             Highcharts.chart('container1', options);
         });
-        $http.get(proxyUrl + "/sonar-service" + "/sonar/widgets/test/test").then(function (response) {
-            Highcharts.chart('container4', response.data);
-        });
-        $http.get(proxyUrl + "/sonar-service" + "/sonar/widgets/test/6").then(function (response) {
+        $http.get(proxyUrl + "/sonar-service" + "/sonar/widgets/6/chart").then(function (response) {
             var options = response.data;
             Highcharts.chart('container2', options);
         });
-    }, 60000);
+        $http.get(proxyUrl + "/sonar-service" + "/sonar/widgets/7/chart").then(function (response) {
+            Highcharts.chart('container3', response.data);
+        });
+    }
+
+    loadData();
+    $interval(loadData, 60000);
 
 });
